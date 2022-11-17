7fe8aa1b2b042f4a519b93df3df7da2161aec2ee
Ninetai <auroraking0112@gmail.com>
2022-11-17 12:53:58 +0200

    Posts CRUD Done

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -59,7 +59,7 @@ function destroy(id) {
                                             type="button"
                                             className="mx-1 px-4 py-2 text-sm text-white bg-red-500 rounded"
                                         >
-                                            Deletef
+                                            Delete
                                         </button>
                                     </td>
                                 </tr>
